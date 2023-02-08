d8a7fd1e118e37491fe5a7835f16b78e71ea2877
ChristianETC1904 <124428126+ChristianETC1904@users.noreply.github.com>
2023-02-08 06:49:54 -0500

    poner los datos de Entidades de clientes

--- Microser.Cliente.Dominio/Entidades/Cliente.cs
+++ Microser.Cliente.Dominio/Entidades/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace Microser.Cliente.Dominio.Entidades
 {
     internal class Cliente
     {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? _id { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public string? NombreCliente { get; set; }
+
+        public int EdadCliente { get; set; }
+
+
+
     }
 }
