43ac485c76a781ecb0ec25955e2945640376479f
ChristianETC1904 <124428126+ChristianETC1904@users.noreply.github.com>
2023-02-08 07:18:41 -0500

    capa infraestructura conexion base de datos

---
+++ Microser.Cliente.Infraestructura/DBRepository/DBRepository.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microser.Cliente.Infraestructura.DBRepository
+{
+    public class DBRepository
+    {
+
+        public MongoClient client:
+
+    }
+}
--- Microser.Cliente.Infraestructura/DBSettings/DBSettings.cs
+++ Microser.Cliente.Infraestructura/DBSettings/DBSettings.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Microser.Cliente.Infraestructura.DBSettings
 {
-    internal class DBSettings
+    public class DBSettings : IDBSettings
     {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Collection { get; set; }
+
+    }
+
+    public interface IDBSettings
+    { 
+        string Server { get; set; }        
+        string Database { get; set; }
+        string Collection { get; set; }
+
     }
 }
